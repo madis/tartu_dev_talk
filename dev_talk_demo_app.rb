e4e82741ff9a225529febba3dd6743a721c6a61f
Madis Nõmme <madis.nomme@gmail.com>
2013-10-04 21:05:41 +0300

    Step closer to letting the exceptions out

--- dev_talk_demo_app.rb
+++ dev_talk_demo_app.rb
@@ -1,8 +1,15 @@
 class DevTalkDemoApp < Sinatra::Base
-  set :show_exceptions, true
-  enable :logging
+  RouteDoesNotExist = Class.new(StandardError)
+  configure do
+    set :show_exceptions, false
+    set :dump_errors, false
+    set :raise_errors, true
+
+    enable :logging
+  end
 
   get '/' do
     slim :index, layout: false
   end
+
 end
--- spec/media_spec.rb
+++ spec/media_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Media on pages' do
   it 'has video element' do
-    visit '/'
+    visit '/porno'
     expect(page).to have_selector('video.source')
   end
 end
